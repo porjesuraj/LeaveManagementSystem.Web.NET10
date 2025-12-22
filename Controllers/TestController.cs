c9f8663bd444cc9deb84e4b50e9bfdd0921620a7
porjesuraj <porjesuraj5@gmail.com>
2025-12-22 08:53:10 +0530

    added code changes for test controller, to test MVC behavior

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel { Name = "Test", Today = DateTime.Now };
+            return View(data);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+    }
+}
